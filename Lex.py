575a3206dd25fc5c6caf228778f29ed4f31916f1
YasinBedirhanSimsek <yasinbedirhansimseq@gmail.com>
2022-01-02 19:47:06 +0300

    ORGANIZED

---
+++ Lex.py
@@ -0,0 +1,89 @@
+from sly import Lexer
+from sly.lex import Token
+
+class CalcLexer(Lexer):
+    # Set of token names. This is always required
+    tokens = { 
+        
+        #Objects
+        ID, NUMBER,
+         
+        #SYMBOLS
+        LPAREN, RPAREN,
+        
+        LCURLY, RCURLY,
+        
+        #OPERATIONS
+        PLUS, MINUS, TIMES, DIVIDE, ASSIGN,
+        
+        BITWISE_OR, BITWISE_AND, BITWISE_NOT,
+        
+        #LOGIC
+        GREATER_THAN, GREATER_THAN_EQ, LOWER_THAN, LOWER_THAN_EQ,
+
+        IS_EQUAL, IS_NOT_EQUAL,
+        
+        #CONDITIONAL
+        IF, ELSE_IF, ELSE,
+
+        #LOOPS
+        WHILE, FOR, BREAK  
+    }
+
+    # String containing ignored characters (between tokens)
+    ignore = ' \t'
+    ignore_comment = r'\#.*'
+    ignore_newline = r'\n+'
+
+    # Regular expression rules for tokens
+
+    #Objects  
+    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    NUMBER  = r'\d+'
+
+    #SYMBOLS
+    LPAREN  = r'\('
+    RPAREN  = r'\)'
+    
+    LCURLY  = r'\{'
+    RCURLY  = r'\}'
+
+    #OPERATIONS
+    PLUS    = r'\+'
+    MINUS   = r'-'
+    TIMES   = r'\*'
+    DIVIDE  = r'/'
+    ASSIGN  = r'='
+
+    BITWISE_OR  = r'\|'
+    BITWISE_AND = r'\&'
+    BITWISE_NOT = r'\!'
+
+    #LOGIC
+    GREATER_THAN_EQ   = r'>='
+    GREATER_THAN      = r'>'
+    
+    LOWER_THAN_EQ  = r'<='
+    LOWER_THAN     = r'<'
+    
+    IS_EQUAL     = r'\?='
+    IS_NOT_EQUAL = r'\!='
+
+    #CONDITIONAL
+    ID['if']      = IF
+    ID['else if'] = ELSE_IF
+    ID['else']    = ELSE
+
+    #LOOPS
+    ID['while'] = WHILE    
+    ID['for']   = FOR 
+    ID['break'] = BREAK
+
+
+    ############################################################
+
+    def NUMBER(self, t:Token):
+        t.value = int(t.value)
+        return t
+
+    
--- hello.py
+++ hello.py
@@ -1,56 +1,35 @@
+from sly.lex import Token
+from Lex import CalcLexer
 from sly import Lexer
 
-class CalcLexer(Lexer):
-    # Set of token names.   This is always required
-    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, LPAREN, RPAREN, LCURLY, RCURLY, IF, OR, GREATERTHAN, AND, NOTEQUAL, LOWERTHAN, LOWER, GREATER  }
-
-    # String containing ignored characters (between tokens)
-    ignore = ' \t'
-
-    # Other ignored patterns
-    ignore_comment = r'\#.*'
-    ignore_newline = r'\n+'
-
-    # Regular expression rules for tokens
-    
-    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    NUMBER  = r'\b(0x[0-9a-fA-F]+|[0-9]+)\b'
-    PLUS    = r'\+'
-    MINUS   = r'-'
-    TIMES   = r'\*'
-    DIVIDE  = r'/'
-    ASSIGN  = r'='
-    LPAREN  = r'\('
-    RPAREN  = r'\)'
-    LCURLY = r'\{'
-    RCURLY = r'\}'
-
-
-    #IF = r'if'
-    # EQUAL = r'\==' #equal does not work it see only one "=" symbol
-    GREATERTHAN = r'<='
-    LOWERTHAN = r'>='
-    LOWER = r'>'
-    GREATER = r'<'
-    OR = r'\|'
-    AND = r'\&'
-    NOTEQUAL = r'!='
-    
-
-    ID['if'] = IF
-
-
 if __name__ == '__main__':
-    data = '''x = 3 + 0x42 anan
-                * (s    # This is a comment
-                    - t) if { asd } | & != > < >= <='''
+
+    data = '''
+    
+    #define a
+    a = 2 + 3
+
+    #define b
+    b = a + 5
+
+    while(b >= 0)
+        a = a - 1
+
+        if(a ?= 0)
+            break
+
+    
+
+    '''
+                 
     lexer = CalcLexer()
 
     token_list = []
 
     for tok in lexer.tokenize(data):
         token_list.append(tok)
-        print(tok)
 
+    tok : Token
+    for tok in token_list:
+        print('(%r, %r)\n' %(tok.type, tok.value) )
 
